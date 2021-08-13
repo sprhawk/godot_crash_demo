c81e93a21dc4683ed589a5a544659083bed991ec
sprhawk <465558+sprhawk@users.noreply.github.com>
2021-08-13 19:17:58 +0800

    initial commit

---
+++ Children/Player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody3D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_forward"):
+		direction.x += 1
+		
+	direction =
